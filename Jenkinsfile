7a9f23763eee053924a1419238d25032a56f1382
Rob Nester <rnester@redhat.com>
2017-08-28 10:49:02 -0400

    test

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,5 +1,4 @@
 #!/usr/bin/env groovy
-
 @Library('daFoo') _
 
 daFoo {
---
+++ src/org/centos/Utils.groovy
@@ -0,0 +1,12 @@
+package org.centos
+
+def call(body) {
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+
+    node() {
+        echo "${config.word} squiggles"
+    }
+}
