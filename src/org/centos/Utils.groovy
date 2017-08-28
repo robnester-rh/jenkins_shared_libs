b5dbbc30d14d3d9d4ff44b8544eba12cf01ed6f3
Rob Nester <rnester@redhat.com>
2017-08-28 12:07:25 -0400

    test

--- src/org/centos/Utils.groovy
+++ src/org/centos/Utils.groovy
@@ -3,3 +3,13 @@ package org.centos
 def makeItBump(word){
     echo ("${word} is bumpin'")
 }
+def call(body) {
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+
+    node() {
+        echo "${config.word} squiggles"
+    }
+}
