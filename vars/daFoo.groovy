4bee541c6af0fc8469cbe42054f03928001ffb6d
Rob Nester <rnester@redhat.com>
2017-08-28 10:40:00 -0400

    test

--- vars/daFoo.groovy
+++ vars/daFoo.groovy
@@ -1,10 +1,3 @@
-def call(body) {
-    def config = [:]
-    body.resolveStrategy = Closure.DELEGATE_FIRST
-    body.delegate = config
-    body()
-
-    node('foo') {
-        echo "${config.word} squiggles"
-    }
+def call(word){
+    return "${word} squiggles"
 }
